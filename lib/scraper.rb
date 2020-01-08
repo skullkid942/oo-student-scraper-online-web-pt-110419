4d54e744059a42be4e1f6a84ad4338625dab1ff7
Bethany Holley <chocolatechipfaced@gmail.com>
2020-01-08 18:10:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,25 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    site = https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
     
+    page = Nokogiri::HTML(open(site))
+    
+    name = page.css"a.card-text-container h4"
+    
+    location = page.css"a.card-text-container p"
+    
+    profile_url = "a.view-profile-div h3"
+    
+    student = {name: name, location: location, profile_url: profile_url}
+    array = [] 
+    array.push(student)
   end
 
   def self.scrape_profile_page(profile_url)
+    site = 
     
+    page = 
   end
 
 end
