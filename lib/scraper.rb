2828bb22bebe09d88f8fc951228c90bc760403b8
Bethany Holley <chocolatechipfaced@gmail.com>
2020-01-08 18:38:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,26 +3,26 @@ require 'pry'
 
 class Scraper
   
-  @@site = https://learn-co-curriculum.github.io/student-scraper-test-page/
+  site = https://learn-co-curriculum.github.io/student-scraper-test-page/
     
-  @@page = Nokogiri::HTML(open(site))
+  page = Nokogiri::HTML(open(site))
   
-  @@profile_url = page.css "a.student-card href"
+  profile_url = page.css "a.student-card href"
 
   def self.scrape_index_page(index_url)
     
-    name = @@page.css"a.card-text-container h4"
+    name = page.css"a.card-text-container h4"
     
-    location = @@page.css"a.card-text-container p"
+    location = page.css"a.card-text-container p"
     
-    profile_url = @@page.css"a.view-profile-div h3"
+    profile_url = page.css"a.view-profile-div h3"
     
     student = {name: name, location: location, profile_url: profile_url}
     array = [] 
     array.push(student)
   end
 
-  def self.scrape_profile_page(@@profile_url)
+  def self.scrape_profile_page(profile_url)
     
     
   end
